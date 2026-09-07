874fddcb4cc9f0fdbba8d67dfc2a10dc427da826
poosmacker <103903987+PooSmacker@users.noreply.github.com>
2026-09-07 08:09:22 +0100

    e

--- src/main/java/me/cortex/voxy/client/core/rendering/building/RenderDataFactory.java
+++ src/main/java/me/cortex/voxy/client/core/rendering/building/RenderDataFactory.java
@@ -10,6 +10,7 @@ import me.cortex.voxy.common.util.UnsafeUtil;
 import me.cortex.voxy.common.world.WorldEngine;
 import me.cortex.voxy.common.world.WorldSection;
 import me.cortex.voxy.common.world.other.Mapper;
+import me.cortex.voxy.common.world.other.VoxelSurface;
 import me.cortex.voxy.commonImpl.VoxyCommon;
 import org.lwjgl.system.MemoryUtil;
 
@@ -201,19 +202,20 @@ public class RenderDataFactory {
         return 0b111L&(0b000_000_010_100L>>(ModelQueries._isTranslucent(metadata)*6+ModelQueries._isDoubleSided(metadata)*3));
     }
 
-    private static long packPartialQuadData(int modelId, long state, long metadata) {
+    private static long packPartialQuadData(int modelId, long state, long metadata, int lodLevel) {
         //This uses hardcoded data to shuffle things
         long lightAndBiome =  (state&((0x1FFL<<47)|(0xFFL<<56)))>>>1;
         lightAndBiome &= ~(ModelQueries._notIsBiomeColoured(metadata) * (0x1FFL << 46));//46 not 47 because is already shifted by 1 THIS WASTED 4 HOURS ;-; aaaaaAAAAAA
         lightAndBiome &= ~(ModelQueries._isFullyOpaque(metadata)*(0xFFL << 55));//If its fully opaque it always uses neighbor light?
 
         long quadData = lightAndBiome;
+        quadData |= VoxelSurface.packQuadHeight(state, lodLevel);
         quadData |= Integer.toUnsignedLong(modelId)<<26;
         quadData |= getQuadTyping(metadata);//Returns the typing already shifted by 1
         return quadData;
     }
 
-    private int prepareSectionData(final long[] rawSectionData) {
+    private int prepareSectionData(final long[] rawSectionData, int lodLevel) {
         final var sectionData = this.sectionData;
         if (sectionData.length!=32*32*32*2) throw new IllegalStateException();
         if (rawSectionData.length!=32*32*32) throw new IllegalStateException();
@@ -247,7 +249,7 @@ public class RenderDataFactory {
 
                         long modelMetadata = this.modelMan.getModelMetadataFromClientId(modelId);
 
-                        sectionData[i2] = packPartialQuadData(modelId, block, modelMetadata);
+                        sectionData[i2] = packPartialQuadData(modelId, block, modelMetadata, lodLevel);
                         sectionData[i2 + 1] = modelMetadata;
 
                         notEmpty |= 1L << j;
@@ -1732,7 +1734,7 @@ public class RenderDataFactory {
         Arrays.fill(this.fluidMasks, 0);
 
         //Prepare everything
-        int neighborMskAndFlags = this.prepareSectionData(section._unsafeGetRawDataArray());
+        int neighborMskAndFlags = this.prepareSectionData(section._unsafeGetRawDataArray(), section.lvl);
         if ((neighborMskAndFlags&(1<<31))!=0) {//We failed to get everything so throw exception
             throw new IdNotYetComputedException(neighborMskAndFlags&((1<<20)-1), true);
         }
